3508c8c4fdba9ed4195bd668cd04b2d06d2ce336
ZinovevVlad <Vladislav937@gmail.com>
2022-11-01 00:39:06 +0300

    finish first scrn

---
+++ dist/js/script.js
@@ -0,0 +1,19 @@
+    const hamburger = document.querySelector('.hamburger'),
+          menu = document.querySelector('.menu'),
+          close = document.querySelector('.menu__close'),
+          overlay = document.querySelector('.menu__overlay');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+    hamburger.classList.add('active');
+});
+
+close.addEventListener('click', () => {
+    menu.classList.remove('active');
+    hamburger.classList.remove('active');
+});
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('active');
+    hamburger.classList.remove('active');
+});
